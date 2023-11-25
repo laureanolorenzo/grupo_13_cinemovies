b383b6aaa8b58fe3294b3f453b22e4c9d4a46321
LudmilaRamella <antonellakusznir17@gmail.com>
2023-11-25 11:24:01 -0300

    cambio el mensaje de servidor corriendo

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
+app.listen(3000,console.log('Servidor corriendo.'));const PORT = 3000;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname,'public')));
 
-app.listen(3000,console.log('Servidor corriendo.'));
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
 
 let homePath = path.resolve(__dirname,'views/home.html')
 app.get('/', (req,res) => res.sendFile(homePath)); //View del home
