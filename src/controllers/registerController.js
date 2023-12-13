e3ce08e1bc5c4325eeed948f7eab50086cd10334
laureanolorenzo <laureanolorenzo@gmail.com>
2023-12-13 15:51:32 -0300

    agrego funcionalidad a register y login

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,8 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+const usersPath = path.join(__dirname,'../datos/users.json')
+
+
 const registerController = {
     registerView(req,res) {
         res.render('registro')
     },
     postRegisterData(req,res) {
+        let usersString = fs.readFileSync(usersPath,{encoding:'utf-8'});
+        let users = JSON.parse(usersString);
+        users.push(req.body);
+        usersToSave = JSON.stringify(users)
+        fs.writeFileSync(usersPath,usersToSave,{encoding: 'utf-8'})
         res.redirect('home')
     }
 }
