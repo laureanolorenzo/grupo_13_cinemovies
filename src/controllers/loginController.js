fd97dd14231e325a9828f4cfabbe9fe08532ed25
laureanolorenzo <laureanolorenzo@gmail.com>
2023-12-13 15:23:15 -0300

    agrego partials de head, header y footer

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,21 +1,26 @@
-let users = [{'username':'test_user', 'password':'test_password'}];
+const fs = require('fs');
+const path = require('path');
 
+let usersString = fs.readFileSync(path.join(__dirname,'../datos/users.json'),{encoding:'utf-8'});
+let users = JSON.parse(usersString);
 
 const loginController = {
     loginView(req,res) {
         res.render('login',{'errormsg':''})
     },
     postLoginData(req,res) {
-        // console.log(req.body.usuario);
         let username = req.body.usuario;
         let password = req.body.contraseña;
         let user = users.filter((u) => u.username === username)[0];
-        if (user && user['password'] == password) {
-            res.redirect('home');
-        } else {
-            res.render('login',{'errormsg':'Los datos no son correctos'})
-        }
+        if (user && user['password'] !== password) {
+            res.render('login',{'errormsg':'La contraseña es incorrecta'});
             
+        } else if (!(user)) {
+            res.render('login',{'errormsg':'No se ha encontrado al usuario'});
+
+        } else if (user && user['password'] === password) {
+            res.redirect('home');
+        }
     }
 }
 module.exports = loginController;
