1dbf1090f522bba11fa5eefa76d2ab5f74a8e7ed
Gonza-Zarlenga <gzarlenga95@gmail.com>
2023-12-20 20:49:53 -0300

    sumamos JSON

--- src/controllers/edicion_productoController.js
+++ src/controllers/edicion_productoController.js
@@ -1,10 +1,15 @@
+const fs = require ('fs');
+const path = require ('path');
+
 const edicion_productoController = {
     edicion_productoView(req,res) {
         res.render('edicion_producto'); // Incluir objeto (que venga de JSON con los datos de cada producto)
     },
 
     edicion_productoProcess(req,res) {
-        res.render('edicion_producto'); // Incluir objeto (que venga de JSON con los datos de cada producto)
+        let movies = fs.readFileSync(path.resolve(__dirname, '../datos/movies.json'), 'utf-8');
+        let moviesObj = JSON.parse(movies);
+
     },
 }
 
