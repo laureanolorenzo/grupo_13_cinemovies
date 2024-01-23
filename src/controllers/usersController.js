7eda0a26f9efd9893a4582b582b230ca81cfc5ac
Gonza-Zarlenga <gzarlenga95@gmail.com>
2024-01-23 20:50:40 -0300

    avance en el registro de usuario

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,12 @@
 const usersController = {
     usersView(req,res) {
         res.render('users'); // Incluir objeto (que venga de JSON con los datos de cada producto)
+    },
+    usersRegister (req,res){
+        return res.send ({
+            body: req.body,
+            file: req.file
+        })
     }
 }
 
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,7 +2,24 @@ const usersController = require('../controllers/usersController');
 const express = require('express');
 const router = express.Router();
 
+const path = require ('path');
+const multer = require ('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb (null,'./public/images/users');
+
+    },
+    filename: (req, file, cb) =>{
+        cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname));
+    }
+})
+
+const uploadFile = multer({storage});
+
 router.get('/usuario', usersController.usersView);
+
+router.post ('/usuario', uploadFile.single('users'), usersController.usersRegister);
 
 
 module.exports = router;
